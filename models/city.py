1fe519fee1c174a242cef9429303aff93b16741a
evertcolombia <evertescalante@gmail.com>
2020-03-30 20:34:04 +0000

    update State and City class

--- models/city.py
+++ models/city.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """This is the city class"""
 from models.base_model import BaseModel
+from sqlalchemy import Column. Integer, String, Sequence
 
-
-class City(BaseModel):
+class City(BaseModel, Base):
     """This is the class for City
     Attributes:
         state_id: The state id
         name: input name
     """
-    state_id = ""
-    name = ""
+
+    __tablename__ = 'cities'
+
+    name = Column(String(128), nullable=True)
+    state_id = Column(String(60), ForeigKey('states.id'), nullable=True)
+
--- models/state.py
+++ models/state.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from models import storage
+from sqlalchemy import Column, Integer, String, Sequence
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=True)
+    cities = relationship('City',
+        backref='state',
+        cascade='delete-orphan')
+
+    @property
+    def cities(self):
+        """ get instances with the same id from 
+            states mapped class
+        """
+
+        cities = storage.all(City)
+        id = self.id
+        instance_list = []
+
+        if cities is not None:
+            for k, v in cities.items():
+                if v.id == id: list.append(v)
+        return instance_list
