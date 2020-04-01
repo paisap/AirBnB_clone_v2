eb5a7eb3c7b6da0b5183ac9809c70e8870003cbc
paisap <santiagoaldana58@gmail.com>
2020-04-01 04:11:37 +0000

    arregle cosas el db

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -21,12 +21,12 @@ class DBStorage:
 
     def __init__(self):
         """ the init method """
-        self.__engine = create_engine('mysql+mysqldb://\
-        {}:{}@{}/{}'.format(os.enviroment.get("HBNB_MYSQL_USER"),
-                            os.environ.get("HBNB_MYSQL_PWD"),
-                            os.environ.get("HBNB_MYSQL_HOST"),
-                            os.environ.get("HBNB_MYSQL_DB")),
-                            pool_pre_ping=True)
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            os.environ.get("HBNB_MYSQL_USER"),
+            os.environ.get("HBNB_MYSQL_PWD"),
+            os.environ.get("HBNB_MYSQL_HOST"),
+            os.environ.get("HBNB_MYSQL_DB")),
+            pool_pre_ping=True)
         if os.environ.get("HBNB_ENV") == 'test':
             Base.drop_all(bind=self.__engine)
 
