903f84c3d077ec2991a9faf04a912363e54fa01b
paisap <santiagoaldana58@gmail.com>
2020-04-02 04:11:03 +0000

    prueba antes de este commit todo bien hasta el 5

--- models/user.py
+++ models/user.py
@@ -18,6 +18,6 @@ class User(BaseModel, Base):
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
     places = relationship('Place', backref='user', cascade='delete')
