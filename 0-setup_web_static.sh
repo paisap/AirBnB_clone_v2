464de4ad37939bb2fe7189773440593aba38aec0
paisap <santiagoaldana58@gmail.com>
2020-04-12 09:14:42 +0000

    task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Prepare your web servers
+sudo apt -y update
+sudo apt -y install nginx
+sudo mkdir /data
+sudo mkdir /data/web_static/
+sudo mkdir /data/web_static/releases
+sudo mkdir /data/web_static/shared
+sudo mkdir /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "37 i\ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+sudo service nginx restart
