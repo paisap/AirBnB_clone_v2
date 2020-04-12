2fb2bf626c660296801854649b552ace7414aed5
paisap <santiagoaldana58@gmail.com>
2020-04-12 09:16:57 +0000

    shellshek

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ sudo mkdir /data/web_static/releases
 sudo mkdir /data/web_static/shared
 sudo mkdir /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-sudo echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html
+echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "37 i\ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
