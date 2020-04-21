1ffd791290b01662531439ce8e94c38e1ed5bbbb
paisap <santiagoaldana58@gmail.com>
2020-04-21 16:51:25 +0000

    task web flask

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def odd_even(n):
+    """ show html """
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """" show html """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def the_number(n):
+    return "%d is a number" % n
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def the_python(text="is cool"):
+    """ documentation """
+    return "Python " + text.replace("_", " ")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def the_c(text):
+    """ documentation"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ hola"""
+    return 'HBNB'
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    print("hola estoy debugeando bien pro :'")
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
