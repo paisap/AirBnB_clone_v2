c7318193e3b3e409a551a16fff7875f01465ce93
paisap <santiagoaldana58@gmail.com>
2020-04-22 20:12:00 +0000

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -17,7 +17,7 @@ def close_storage():
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """ display a HTML page: """
-    states = storage.all(States)
+    states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
 
